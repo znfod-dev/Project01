23d1fbd5016dfe3066f67b20706521d985cce3f6
김삼현 <sama73@naver.com>
2019-02-09 15:59:23 +0900

    1. 오픈라이센스 웹화면 처리 완료

--- Project01/Project01/ViewControllers/Setting/OpenSourceViewController.swift
+++ Project01/Project01/ViewControllers/Setting/OpenSourceViewController.swift
@@ -8,30 +8,41 @@
 
 import UIKit
 
-class OpenSourceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewDidLoad() {
+class OpenSourceViewController: UIViewController {
+
+	@IBOutlet weak var vContent: UIView?
+	var webviewVC: BaseWebViewController?
+
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+		let storyboard: UIStoryboard? = AppDelegate.sharedNamedStroyBoard("Common") as? UIStoryboard
+		webviewVC = storyboard?.instantiateViewController(withIdentifier: "BaseWebViewController") as? BaseWebViewController
+		webviewVC!.webviewType = .kWebviewTypeView
+		webviewVC!.strUrlTrigger = (URL(fileURLWithPath: Bundle.main.path(forResource: "OpenSourceLicense", ofType: "html", inDirectory: "www") ?? "")).absoluteString
+		vContent!.addSubview(webviewVC!.view)
+		addChild(webviewVC!)
     }
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		let frame = vContent?.bounds
+		webviewVC!.view.frame = frame!
+	}
     
     @IBAction func backBtnClicked(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+		
+		// 뒤로 갈수 있는 상태면 history back
+		if let webView = webviewVC?.webView {
+			if webView.canGoBack == true {
+				// web에서 히스토리백 처리 맡길려고...
+				webView.goBack()
+				return
+			}
+		}
+		
+		self.navigationController?.popViewController(animated: true)
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "OpenSourceTableCell") as! UITableViewCell
-        return cell
-    }
-
 }
