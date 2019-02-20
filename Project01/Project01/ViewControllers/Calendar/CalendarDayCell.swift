2fa1c4326d8857087b472e8d189d33eaed363787
김삼현 <sama73@naver.com>
2019-02-20 17:11:09 +0900

    1. 월간일정 화면 수정

--- Project01/Project01/ViewControllers/Calendar/CalendarDayCell.swift
+++ Project01/Project01/ViewControllers/Calendar/CalendarDayCell.swift
@@ -12,9 +12,7 @@ class CalendarDayCell: UICollectionViewCell {
 	
 	@IBOutlet weak var vHolidayCircle: UIView!
 	@IBOutlet weak var lbDay: UILabel!
-    @IBOutlet weak var vToDay: UIView!
-	@IBOutlet weak var vSelectedCell: UIView!
-    @IBOutlet weak var vHoliday: UIView!
+    @IBOutlet weak var vToDay: RoundLineView!
     @IBOutlet weak var lbHoliday: UILabel!
     @IBOutlet weak var lbDayLunar: UILabel!
 
@@ -45,148 +43,118 @@ class CalendarDayCell: UICollectionViewCell {
         if infoData.index(forKey: "dayLunar") != nil {
             dayLunar = infoData["dayLunar"] as! Int
         }
-        
-        if monthLunar == 0 || dayLunar == 0 {
-            lbDayLunar.isHidden = true
-        }
-        else {
-			// 이전 음력설정과 변경유무 체크
-			let isLunarCalendar = !CommonUtil.getUserDefaultsBool(forKey: kBool_isLunarCalendar)
-            lbDayLunar.isHidden = isLunarCalendar
-            lbDayLunar.text = String(format: "%02d.%02d", monthLunar, dayLunar)
-        }
-        
-        lbDay.text = "\(day)"
-        
-        // 현재달
-        if monthDirection == 0 {
-            // 오늘일 경우
-            if CalendarManager.getTodayIndex() == cellIndex {
-                lbDay.textColor = UIColor.white
-                vToDay.isHidden = false
-            }
-            else {
-                lbDay.textColor = UIColor(hex: 0x2E2E39)
-                vToDay.isHidden = true
-            }
-        }
-        // -1 or 1 이면 이전달, 다음달
-        else {
-            lbDay.textColor = UIColor(hex: 0xBBBBBB)
-            vToDay.isHidden = true
-        }
 		
-        // 공휴일 체크
-        if isHoliday == true {
-			// 공휴일 동그라미
-//			if CommonUtil.isEmpty(holidayName as AnyObject) == false {
-//				vHolidayCircle.isHidden = false
-//			}
-//			else {
-//				vHolidayCircle.isHidden = true
-//			}
+		lbDay.text = "\(day)"
+
+		var colorDay = 0xBBBBBB
+
+		// 현재달
+		if monthDirection == 0 {
+			var orgColorDay = 0x1E1E1E
+			var colorHolidayCircle = 0xF9553C
 			
-			// 현재달
-			if monthDirection == 0 {
-				// 오늘일 경우
-				if CalendarManager.getTodayIndex() == cellIndex {
-//					vHolidayCircle.backgroundColor = UIColor(hex: 0xffffff)
-					lbDay.textColor = UIColor(hex: 0xffffff)
-					vToDay.backgroundColor = UIColor(hex: 0x8578DF)
-					vToDay.isHidden = false
+			vHolidayCircle.isHidden = true
+			lbHoliday.isHidden = true
+			lbDayLunar.isHidden = true
+			vPageControl.isHidden = true
+
+			// 공휴일 체크
+			if isHoliday == true {
+				orgColorDay = 0xF9553C
+				
+				// 공휴일 동그라미
+				if CommonUtil.isEmpty(holidayName as AnyObject) == false {
+					vHolidayCircle.isHidden = false
+					vHolidayCircle.backgroundColor = UIColor(hex: orgColorDay)
 				}
 				else {
-//					vHolidayCircle.backgroundColor = UIColor(hex: 0xF9553C)
-					lbDay.textColor = UIColor(hex: 0xF9553C)
-					vToDay.isHidden = true
+					vHolidayCircle.isHidden = true
 				}
 			}
-			// -1 or 1 이면 이전달, 다음달
-			else {
-				// 오늘일 경우
-				if CalendarManager.getTodayIndex() == cellIndex {
-//					vHolidayCircle.backgroundColor = UIColor(hex: 0xffffff)
-					lbDay.textColor = UIColor(hex: 0xffffff)
-					vToDay.backgroundColor = UIColor(hex: 0x8578DF)
-					vToDay.isHidden = false
-				}
-				else {
-//					vHolidayCircle.backgroundColor = UIColor(hex: 0xF9553C)
-					lbDay.textColor = UIColor(hex: 0xBBBBBB)
-					vToDay.isHidden = true
-				}
-			}
-        }
-        else {
-			// 공휴일 동그라미
-//			vHolidayCircle.isHidden = true
 			
-			// 현재달
-			if monthDirection == 0 {
-				// 오늘일 경우
-				if CalendarManager.getTodayIndex() == cellIndex {
-					lbDay.textColor = UIColor.white
-					vToDay.backgroundColor = UIColor(hex: 0x8578DF)
-					vToDay.isHidden = false
-				}
-				else {
-					lbDay.textColor = UIColor(hex: 0x2E2E39)
-					vToDay.isHidden = true
-				}
-			}
-			// -1 or 1 이면 이전달, 다음달
-			else {
-				// 오늘일 경우
-				if CalendarManager.getTodayIndex() == cellIndex {
-					lbDay.textColor = UIColor.white
-					vToDay.backgroundColor = UIColor(hex: 0x8578DF)
-					vToDay.isHidden = false
-				}
-				else {
-					lbDay.textColor = UIColor(hex: 0xBBBBBB)
-					vToDay.isHidden = true
-				}
-			}
-        }
-		
-		// 셀선택
-		if CalendarManager.selectedCell == cellIndex {
-//			vSelectedCell.isHidden = false
+			colorDay = orgColorDay
 			
-			// 선택했을때 공휴일 표시
-			if CommonUtil.isEmpty(holidayName as AnyObject) == false {
-				vHoliday.isHidden = false
-				lbHoliday.text = holidayName
+			// 오늘일 경우
+			if CalendarManager.getTodayIndex() == cellIndex {
+				vToDay.isHidden = false
+				
+				vToDay.borderWidth = 0.0
+				vToDay.backgroundColor = UIColor(hex: 0x8578DF)
+				
+				colorDay = 0xFFFFFF
+				colorHolidayCircle = colorDay
 			}
 			else {
-				vHoliday.isHidden = true
+				vToDay.isHidden = true
+			}
+			
+			// 셀선택
+			if CalendarManager.selectedCell == cellIndex {
+				vToDay.isHidden = false
+				vToDay.borderWidth = 2.0
+				vToDay.backgroundColor = UIColor.clear
+				
+				// 공휴일 일때...
+				if isHoliday == true {
+					colorHolidayCircle = 0xF9553C
+					vToDay.borderColor = UIColor(hex: colorHolidayCircle)
+					
+					// 공휴일명 세팅
+					lbHoliday.isHidden = false
+					lbHoliday.text = holidayName
+				}
+				else {
+					vToDay.borderColor = UIColor(hex: 0x8578DF)
+				}
+				
+				colorDay = orgColorDay
+			}
+
+			vHolidayCircle.backgroundColor = UIColor(hex: colorHolidayCircle)
+			
+			// 음력 표시
+			if monthLunar == 0 || dayLunar == 0 {
+				lbDayLunar.isHidden = true
+			}
+			else {
+				// 이전 음력설정과 변경유무 체크
+				let isLunarCalendar = !CommonUtil.getUserDefaultsBool(forKey: kBool_isLunarCalendar)
+				lbDayLunar.isHidden = isLunarCalendar
+				lbDayLunar.text = String(format: "%02d.%02d", monthLunar, dayLunar)
+			}
+			
+			// Todo List
+			if todoCount == 0 {
+				vPageControl.isHidden = true
+			}
+			// 공휴일명 표시하고 있으면...
+			else if lbHoliday.isHidden == true {
+				// 최대 3개까지 표시해준다.
+				if todoCount > 3 {
+					todoCount = 3
+				}
+				
+				let gapWidth: CGFloat = 5.0
+				// 페이지 컨트롤
+				vPageControl.gapWidth = gapWidth
+				vPageControl.normalItem = UIColor(hex: 0x8578DF)
+				vPageControl.selectedItem = UIColor(hex: 0x8578DF)
+				vPageControl.setTotalPage(todoCount)
+				// 10px : 이미지 크기 8px 이미지 간격
+				vPageControlWidthConstraint.constant = (vPageControl.frame.size.height * CGFloat(todoCount) + gapWidth * CGFloat(todoCount-1))
+				
+				vPageControl.isHidden = false
 			}
 		}
+		// -1 or 1 이면 이전달, 다음달
 		else {
-//			vSelectedCell.isHidden = true
-			vHoliday.isHidden = true
-		}
-		
-		// Todo List
-		if todoCount == 0 {
+			vHolidayCircle.isHidden = true
+			vToDay.isHidden = true
+			lbHoliday.isHidden = true
+			lbDayLunar.isHidden = true
 			vPageControl.isHidden = true
 		}
-		else {
-            // 최대 3개까지 표시해준다.
-            if todoCount > 3 {
-                todoCount = 3
-            }
-            
-			let gapWidth: CGFloat = 5.0
-			// 페이지 컨트롤
-			vPageControl.gapWidth = gapWidth
-			vPageControl.normalItem = UIColor(hex: 0x8578DF)
-			vPageControl.selectedItem = UIColor(hex: 0x8578DF)
-			vPageControl.setTotalPage(todoCount)
-			// 10px : 이미지 크기 8px 이미지 간격
-			vPageControlWidthConstraint.constant = (vPageControl.frame.size.height * CGFloat(todoCount) + gapWidth * CGFloat(todoCount-1))
-			
-			vPageControl.isHidden = false
-		}
+		
+		lbDay.textColor = UIColor(hex: colorDay)
     }
 }
