ba10a005297a156b493d33a4ba924fefc3df5455
김삼현 <sama73@naver.com>
2019-03-16 17:08:00 +0900

    1. 런처, 인트로 화면 처리

--- Project01/Project01/ViewControllers/IntroViewController.swift
+++ Project01/Project01/ViewControllers/IntroViewController.swift
@@ -10,10 +10,6 @@ import UIKit
 
 class IntroViewController: UIViewController {
 
-	@IBOutlet weak var ivCloud: UIImageView!
-	@IBOutlet weak var ivRefresh: UIImageView!
-	var ivRefreshMask: UIImageView!
-	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,12 +17,6 @@ class IntroViewController: UIViewController {
 		// 375화면 기준으로 스케일 적용
 		let scale: CGFloat = DEF_WIDTH_375_SCALE
 		view.transform = view.transform.scaledBy(x: scale, y: scale)
-
-		ivRefreshMask = UIImageView(image: UIImage(named: "img_refresh_mask"))
-		ivRefresh.mask = ivRefreshMask
-		
-		// 리프레쉬 이미지 회전
-		rotateAnimation()
 
 		// NotificationCenter에 등록하는 것
 		NotificationCenter.default.addObserver(self,  // 추천 항목
@@ -43,18 +33,6 @@ class IntroViewController: UIViewController {
 		
 		// Stop listening notification
 		NotificationCenter.default.removeObserver(self, name: Notification.Name("CloudLoadComplete"), object: nil)
-	}
-	
-	// 리프레쉬 이미지 회전
-	func rotateAnimation(duration: CFTimeInterval = 2.0) {
-		let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
-		rotateAnimation.fromValue = 0.0
-		rotateAnimation.toValue = CGFloat(.pi * 2.0)
-		rotateAnimation.duration = duration
-		rotateAnimation.repeatCount = Float.greatestFiniteMagnitude;
-		
-		self.ivRefreshMask!.layer.add(rotateAnimation, forKey: nil)
-		//		self.ivLoadding!.layer.add(rotateAnimation, forKey: nil)
 	}
 	
     /*
