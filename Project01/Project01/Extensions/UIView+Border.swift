90cdee9b2ccf7aef65c0705777d92eb8314f9906
byunsangjin <qustkdwls1@naver.com>
2019-03-19 22:23:05 +0900

    UIView+Border 분리

---
+++ Project01/Project01/Extensions/UIView+Border.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Border.swift
+//  Project01
+//
+//  Created by Byunsangjin on 19/03/2019.
+//  Copyright © 2019 Znfod. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        if #available(iOS 11.0, *) {
+            clipsToBounds = true
+            layer.cornerRadius = radius
+            layer.maskedCorners = CACornerMask(rawValue: corners.rawValue)
+        } else {
+            let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            layer.mask = mask
+        }
+    }
+}
